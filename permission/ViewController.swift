78e5c2c6f9a46b49e936b5b580cf0b399b469621
boztopuz <boztopuz@gmail.com>
2023-09-20 15:15:47 +0300

    Initial Commit

---
+++ permission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  permission
+//
+//  Created by Burak Öztopuz on 20.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
